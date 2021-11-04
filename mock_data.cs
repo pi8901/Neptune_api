e1a45a37a01f15a4802ccbb530912a6cf0d68c2c
pi8901 <pi8901@gmx.de>
2021-11-04 22:20:16 +0100

    Test

--- mock_data.cs
+++ mock_data.cs
@@ -1,4 +1,5 @@
 using Neptune.Models;
+using Neptune.Data;
 
 namespace Neptune
 {
@@ -6,7 +7,14 @@ namespace Neptune
     {
         public mock_data()
         {
-            Option option_a = new Option { };
+            DatabaseDbContext context = new DatabaseDbContext();
+            DateTime d = DateTime.Now;
+            Console.Write(d);
+            Parameter[] x = new Parameter[];
+            Option option_a = new Option { archived = false, updated = d,
+             created = d, Id = 1, title = "option_a", description = "This is option_a", datatype = 1, parameter = x  };
+            context.Add(option_a);
+            context.SaveChanges();
         }
     }
 

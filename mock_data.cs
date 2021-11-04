44eed0b53707a08a3f312aa64a3d632cc5461c6c
pi8901 <pi8901@gmx.de>
2021-11-04 21:48:25 +0100

    added Mock-data class

---
+++ mock_data.cs
@@ -0,0 +1,13 @@
+using Neptune.Models;
+
+namespace Neptune
+{
+    public class mock_data
+    {
+        public mock_data()
+        {
+            Option option_a = new Option { };
+        }
+    }
+
+}
